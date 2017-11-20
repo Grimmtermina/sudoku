93fa06984b6ef23f2afab455dc57e6a19fe610fa
Grimmtermina <werdna.rellim@gmail.com>
2017-11-20 09:28:59 -0700

    Session handlers added
    Added session handler code to controller.php and puzzle.php. Currently causing problems ("Error connecting to server") when loading the page, trying to find out why. Added "loggedinbtn" to css.

--- controller.php
+++ controller.php
@@ -1,1 +1,84 @@
 <?php
+// AUTHOR: Andrew Miller; DATE: 11/8/2017
+
+include 'model.php';
+
+session_start();
+
+unset($_SESSION['loginError']);
+unset($_SESSION['regError']);
+
+
+// DEBUGGING CODE:
+// Login button session handler
+if(isset($_POST['usn']) && isset($_POST['pass'])) {
+    // Set session based on usn (if exists)
+    if ($_POST['usn'] == 'andrew' && $_POST['pass'] == 'miller') {
+        $_SESSION['user'] = $_POST['usn'];
+        header('Location: puzzle.php');
+    }
+    // Return to login page with error (if doesn't exist)
+    else {
+        $_SESSION['loginError'] = 'Invalid USN or pass.';
+        header('Location: login.php');
+    }
+    unset($_SESSION['usn']);
+    unset($_SESSION['pass']);
+}
+
+// // Login button session handler
+// if(isset($_POST['usn']) && isset($_POST['pass'])) {
+//     $found = -1;
+//     // Search for existing usn
+//     for($i = 0; $i < count($userArr); $i++) {
+//         if($userArr[$i]['username'] == $_POST['usn']) {
+//             $found = $i;
+//         }
+//     }
+//     // Set session based on usn (if exists)
+//     if ($found != -1 && (password_verify($_POST['pass'],$userArr[$found]['hash']) == 1)) {
+//         $_SESSION['user'] = $_POST['usn'];
+//         header('Location: puzzle.php');
+//     }
+//     // Return to login page with error (if doesn't exist)
+//     else {
+//         $_SESSION['loginError'] = 'Invalid USN or pass.';
+//         header('Location: login.php');
+//     }
+//     unset($_SESSION['usn']);
+//     unset($_SESSION['pass']);
+// }
+
+// Registration session handler
+if(isset($_POST['regusn']) && isset($_POST['regpass'])) {
+    $found = -1;
+    // Check for existing usn
+    for($i = 0; $i < count($userArr); $i++) {
+        if($userArr[$i]['username'] == $_POST['regusn']) {
+            $found++;
+        }
+    }
+    // Return to registration page with error (if usn already exists)
+    if($found != -1) {
+        $_SESSION['regError'] = 'USN already exists in database. Choose different USN';
+        header('Location: register.php');
+    }
+    // Add new usn with associated password to the database (if doesn't exist)
+    else {
+        $hash = password_hash($_POST['regpass'], PASSWORD_DEFAULT);
+        $theDBA->addUserToDB($_POST['regusn'],$hash);
+        header('Location: puzzle.php');
+    }
+    unset($_SESSION['regusn']);
+    unset($_SESSION['regpass']);
+}
+
+// Difficulty settings handler
+// TODO: Decide on whether to perform through a separate page or a dropdown menu or something
+
+// Logout session handler
+if(isset( $_POST['logout']) && $_POST['logout'] == 'Logout') {
+    session_destroy();
+    header('Location: puzzle.php');
+}
+?>
--- puzzle.php
+++ puzzle.php
@@ -12,11 +12,25 @@ password. Registration is done through the login page right now. -->
 <title>Sudoku</title>
 </head>
 <body onload="generatePuzzle();">
+	<?php 
+	   require_once './model.php';
+	   session_start();
+	?>
 	<h1>SUDOKU CHALLENGE</h1>
 	<div class="btnbar">
     	<a class="btn" href="login.php">Login</a>
     	<a class="btn" href="highScore.php">View High Scores</a>
+    	<?php
+    	// Session-specific button functionality
+        if(isset($_SESSION['user'])) {
+            echo '<form action="controller.php" method="POST">';
+            echo '<input class="btn" type="submit" name="difficulty" value="Change Difficulty">';
+            echo '<br><input class="loggedinbtn" type="submit" name="logout" value="Logout">';
+            echo '</form><br>';
+        }
+        ?>
 	</div><br>
+    
 	<?php 
 	// Automates the writing of 81 boxes organized into 9 subsections
 	echo '<div class="puzzle"><div class="puzzleSection">';
@@ -29,14 +43,18 @@ password. Registration is done through the login page right now. -->
 	        echo '</div>';
 	    }
 	}
-	echo '</div>'
+	echo '</div>';
 	?>
 	
 	<script>
 	var array = [];
 	var puzzleArray = [];
+	
+	// TODO: Change function to allow random number generation w/in sudoku guidelines
 	function generatePuzzle() {
 		var boxNum = "";
+
+		// FIXME: Currently just populates the boxes with their box #
 		for (var i = 0; i < 81; i++) {
 			boxNum = (i + 1).toString();
 			puzzleArray[i] = document.getElementById(boxNum);
